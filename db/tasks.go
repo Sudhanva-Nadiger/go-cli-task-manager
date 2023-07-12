88e38e5725b1d099031b1f0525cafa72503aca2e
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-07-12 20:14:10 +0530

    feat: delete task

--- db/tasks.go
+++ db/tasks.go
@@ -68,6 +68,13 @@ func AllTasks() ([]Task, error) {
 	return tasks, nil
 }
 
+func DeleteTask(key int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
+}
+
 func itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
